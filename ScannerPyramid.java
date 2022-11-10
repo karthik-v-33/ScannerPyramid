418783314b5f8686708d5786969ae00e01403866
Karthik <105358710+karthik-v-33@users.noreply.github.com>
2022-11-10 15:02:49 +0530

    Add files via upload

---
+++ ScannerPyramid.java
@@ -0,0 +1,33 @@
+package com.GitHub_Updates;
+
+import java.util.Scanner;
+
+public class ScannerPyramid {
+
+	public static void main(String[] args) {
+		Scanner sc;
+		sc=new Scanner(System.in);
+		System.out.print("Enter the Number :");
+		int a=sc.nextInt();
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.println(" " + a);
+		System.out.print(" ");
+		System.out.print(" " + a);
+		System.out.print(a);
+		System.out.println(a);
+		System.out.print(" " + a);
+		System.out.print(a);
+		System.out.print(a);
+		System.out.print(a);
+		System.out.println(a);
+		System.out.print(a);
+		System.out.print(a);
+		System.out.print(a);
+		System.out.print(a);
+		System.out.print(a);
+		System.out.print(a);
+		System.out.println(a);
+	}
+
+}
